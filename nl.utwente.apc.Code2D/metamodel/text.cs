655da5d288e9f94ca88dcf2c2699061a6c65f280
damiaan91 <damiaan91@gmail.com>
2013-06-03 15:40:09 +0200

    Added AddObject, AddTerrain, Triggers, Conditions and Events (partial)

--- nl.utwente.apc.Code2D/metamodel/text.cs
+++ nl.utwente.apc.Code2D/metamodel/text.cs
@@ -21,15 +21,39 @@ TOKENSTYLES {
 	"WorldBlocks" COLOR #7F0055, BOLD;
 	"Trap" COLOR #7F0055, BOLD;
 	"Scenery" COLOR #7F0055, BOLD;
+	"Name" COLOR #7F0055, BOLD;
+	"ObjectInstance" COLOR #7F0055, BOLD;
+	"Object" COLOR #7F0055, BOLD;
+	"X1" COLOR #7F0055, BOLD;
+	"X2" COLOR #7F0055, BOLD;
+	"Y1" COLOR #7F0055, BOLD;
+	"Y2" COLOR #7F0055, BOLD;
+	"x" COLOR #7F0055, BOLD;
+	"TerrainInstance" COLOR #7F0055, BOLD;
+	"Event" COLOR #7F0055, BOLD;
+	"EventTriggers" COLOR #7F0055, BOLD;
+	"Trigger" COLOR #7F0055, BOLD;
+	"Conditions" COLOR #7F0055, BOLD;
+	"Value" COLOR #7F0055, BOLD;
+	"Condition" COLOR #7F0055, BOLD;
+	"Left" COLOR #7F0055, BOLD;
+	"Right" COLOR #7F0055, BOLD;
+	"Operator" COLOR #7F0055, BOLD;
 }
 
 RULES {
 	Game ::= "Game" Name[] "{" (GameObjects | GameWorld)* "}";
 	Player ::= "Player" Name[] "{" ("Texture" ":" Texture[])* "}";
-	NPC ::= "NPC" Name[] (":" Extends[])* "{" ("Texture" ":" Texture[] ";" | "Behaviour" ":" Behaviour[Friendly:"Friendly", Neutral:"Neutral", Hostile:"Hostile"] ";")* "}";
+	NPC ::= "NPC" Name[] (":" Extends[])* "{" ("Texture" ":" Texture[] ";" | "Behaviour" ":" Behaviour[FRIENDLY:"Friendly", NEUTRAL:"Neutral", HOSTILE:"Hostile"] ";")* "}";
 	Item ::= "Item" Name[] "{" ("Texture" ":" Texture[] ";")* "}";
-	World ::= "World" "{" ("Name" ":" Name['"','"'] | "Terrain" ":" Terrain['"','"'] | "WorldBlocks" ":" WorldBlocks)* "}";
+	World ::= "World" "{" ("Name" ":" Name['"','"'] | "WorldBlocks" ":" WorldBlocks | InstancePositions | WorldTriggers | WorldEvents)* "}";
 	Terrain ::= CanStand["CanStand" : ""] CanHaveItem["CanHaveItem" : ""] "Terrain" "{" "}";
 	Trap ::= "Trap" "{" ("Name" ":" Name['"','"'] | "Texture" ":" Texture['"','"'])* "}";
 	Scenery ::= "Scenery" "{" ("Name" ":" Name['"','"'] | "Texture" ":" Texture['"','"'])* "}";
+	ObjectInstance ::= ("AddObject:" Object[] (X1[INTEGER] "x" Y1[INTEGER])* ("<>" X2[INTEGER] "x" Y2[INTEGER])*)* ";";
+	TerrainInstance ::= ("AddTerrain:" Terrain[] (X1[INTEGER] "x" Y1[INTEGER])* ("<>" X2[INTEGER] "x" Y2[INTEGER])*)* ";";
+	Event ::= "Event" Name[] "on" EventTriggers[] ("," EventTriggers[]) "{" "}";
+	Value ::= ((Negation["!" : ""])? Value[]);
+	Trigger ::= "Trigger" Name[] "{" Conditions "}";
+	Condition ::= (Left Operator[COLLIDES:"collides", AND:"&", OR:"|"] Right)*;
 }
